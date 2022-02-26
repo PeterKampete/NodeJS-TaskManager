b853474982514144747865b31977659e84d901a8
PeterKampete <peterkampete01@gmail.com>
2022-02-26 16:47:34 +0100

    finished with the task manager app

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-const PORT = 9000;
+const port = process.env.PORT || 9000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(PORT, function () {
-      console.log(`Server listening on http://localhost:${PORT}...`);
+    app.listen(port, function () {
+      console.log(`Server listening on http://localhost:${port}...`);
     });
   } catch (error) {
     console.log(error);
